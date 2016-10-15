d57c824b214cf555c68c13d6c8d17c76f449a878
Joao Paulo <jpc.araujo@hotmail.com>
2016-10-14 23:24:47 -0300

    Estrutura Set data

--- js/main.js
+++ js/main.js
@@ -170,3 +170,33 @@ var Client = function (_Pessoa) {
 var client = new Client(1, "Fulano", "a@a.com", "1234-5678");
 console.log(client.toString());
 "use strict";
+
+var color = new Set();
+color.add("azul").add("verde").add("azul");
+
+if (color.size === 2 && color.has("azul")) {
+    var _iteratorNormalCompletion = true;
+    var _didIteratorError = false;
+    var _iteratorError = undefined;
+
+    try {
+        for (var _iterator = color.values()[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+            var key = _step.value;
+
+            console.log(key);
+        }
+    } catch (err) {
+        _didIteratorError = true;
+        _iteratorError = err;
+    } finally {
+        try {
+            if (!_iteratorNormalCompletion && _iterator.return) {
+                _iterator.return();
+            }
+        } finally {
+            if (_didIteratorError) {
+                throw _iteratorError;
+            }
+        }
+    }
+}
--- src/7_set_data_structure.js
+++ src/7_set_data_structure.js
@@ -1,1 +1,9 @@
+let color = new Set();
+color.add("azul").add("verde").add("azul");
 
+
+if(color.size === 2 && color.has("azul")){
+    for(let key of color.values()){
+        console.log(key);
+    }
+}
